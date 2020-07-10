1dbc9b021595d2702baa692384b6122de536225b
Harshini3 <Harshini3@github.com>
2020-07-10 10:30:12 +0530

    blog on pandemic
    simple blog on 2020

---
+++ src/containers/ContactUS/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const ContactUS = (props) => {
+  return(
+    <div className="contact">
+      Contactus on email xyz@gmail.com
+    </div>
+   )
+
+ }
+
+export default ContactUS
