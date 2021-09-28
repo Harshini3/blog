8facebd8b6b18783d8c5a49fc42e6233493e5758
Harshini3 <n.harshini.harshini@gmail.com>
2021-09-28 07:17:51 +0530

    blog

---
+++ src/containers/Aboutus/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+
+const AboutUS = (props) => {
+  return(
+    <div className="about">
+This Blog is all about the fitness,diet,mental heath related topics posted during this tough time of pandemic
+    </div>
+   )
+
+ }
+
+export default AboutUS
